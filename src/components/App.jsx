2310f3cedbd323bbd081b5dac45d74e7e0e1b06d
yerma4ok <yerma4ok13@gmail.com>
2023-02-13 18:39:03 +0100

    App func

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { React, Component } from 'react';
+import { useState, useEffect  } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import * as API from '../pictures/api';
@@ -9,32 +9,35 @@ import Loader from './Loader';
 import Button from './Button';
 import Modal from './Modal';
 
-class App extends Component {
-  state = {
-    searchImage: '',
-    images: [],
-    page: 1,
-    status: 'idle',
-    showModal: false,
-    largeImageModal: null,
-  };
+function App() {
+  const [searchImage, setSearchImage] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [status, setStatus] = useState('idle');
+  const [showModal, setShowModal] = useState(false);
+  const [largeImageModal, setLargeImageModal] = useState(null);
+  // state = {
+  //   searchImage: '',
+  //   images: [],
+  //   page: 1,
+  //   status: 'idle',
+  //   showModal: false,
+  //   largeImageModal: null,
+  // };
 
-  async componentDidUpdate(_, prevState) {
-    const currentSearch = prevState.searchImage;
-    const newSearch = this.state.searchImage;
-    const currentPage = prevState.page;
-    const nextPage = this.state.page;
-    const isUpdate = currentSearch !== newSearch || currentPage !== nextPage;
+  useEffect(() => {
+    if (!searchImage) {
+      return;
+    }
 
-    if (isUpdate) {
-      this.setState({ status: 'pending' });
+    async function render() {
+      setStatus('pending');
 
       try {
-        const imageList = await API.getImages(newSearch, nextPage);
-        this.setState(prevState => ({
-          images: [...prevState.images, ...imageList],
-          status: 'resolved',
-        }));
+        const imageList = await API.getImages(searchImage, page);
+        setImages([...images, ...imageList]);
+        setStatus('resolved');
+
         if (imageList.length === 0) {
           toast.error(
             'Sorry, there are no images matching your search query. Please, try again.'
@@ -42,47 +45,43 @@ class App extends Component {
         }
       } catch (error) {
         toast.error('Something went wrong. Please, reload the page.');
-        this.setState({ status: 'rejected' });
+        setStatus('rejected');
       }
     }
-  }
+    render();
+  }, [images, page, searchImage]);
 
-  onFormSubmit = searchImage => {
-    this.setState({ searchImage, images: [], page: 1 });
+  const onFormSubmit = searchImage => {
+    setSearchImage(searchImage);
+    setImages([]);
+    setPage(1);
   };
 
-  onLoadBtnClick = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+  const onLoadBtnClick = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
-  onToggleModal = largeImageURL => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-    }));
-    this.setState({ largeImageModal: largeImageURL });
+  const onToggleModal = largeImageURL => {
+    setShowModal(!showModal);
+    setLargeImageModal(largeImageURL);
   };
 
-  render() {
-    const { images, status, showModal, largeImageModal } = this.state;
-
-    return (
-      <>
-        <Searchbar onSubmit={this.onFormSubmit} />
-        {images.length > 0 && (
-          <ImageGallery items={images} onClick={this.onToggleModal} />
-        )}
-        {status === 'pending' && <Loader />}
-        {images.length >= 12 && <Button onClick={this.onLoadBtnClick} />}
-        {showModal && (
-          <Modal onClose={this.onToggleModal}>
-            <img src={largeImageModal} alt="" />
-          </Modal>
-        )}
-        <ToastContainer autoClose={3000} position="top-right" />
-      </>
-    );
-  }
+  return (
+    <>
+      <Searchbar onSubmit={onFormSubmit} />
+      {images.length > 0 && (
+        <ImageGallery items={images} onClick={onToggleModal} />
+      )}
+      {status === 'pending' && <Loader />}
+      {images.length >= 12 && <Button onClick={onLoadBtnClick} />}
+      {showModal && (
+        <Modal onClose={onToggleModal}>
+          <img src={largeImageModal} alt="" />
+        </Modal>
+      )}
+      <ToastContainer autoClose={3000} position="top-right" />
+    </>
+  );
 }
+
 export default App;
